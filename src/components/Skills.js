904f8a9a8f7f79909267583e6f0fae902c4e8455
florpalma1 <tuemail@example.com>
2024-11-26 10:29:25 -0500

    agregue comentarios y cambios en la secciones de skills o datos informativos del torneo

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,6 +7,8 @@ import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
 
+/*Agregar imagenes representativa de cada item informativo*/
+
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
@@ -34,24 +36,28 @@ export const Skills = () => {
             <div className="row">
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
+                        <h2>Torneo de Verano 2025</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Cantidad de equipos inscriptos</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>cantidad de canchas</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>numero de edicion</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>categorias</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Premios</h5>
                             </div>
                         </Carousel>
                     </div>
